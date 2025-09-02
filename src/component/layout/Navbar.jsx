f3bad011dec65bf247a7f5a0145c559ecd3091f6
Ahmad-Padarwala <ahmadpadarwala4@gmail.com>
2025-09-02 17:30:29 +0530

    add colors

--- src/component/layout/Navbar.jsx
+++ src/component/layout/Navbar.jsx
@@ -6,32 +6,32 @@ const Navbar = () => {
     return (
 
         <>
-            <nav class="bg-white border-gray-200 dark:bg-gray-900">
-                <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
-                    <Link href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
-                        <Image src={Logo} class="w-[70px]" alt="Flowbite Logo" />
+            <nav className="bg-white border-gray-200 dark:bg-gray-900">
+                <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
+                    <Link href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+                        <Image src={Logo} className="w-[70px]" alt="Flowbite Logo" />
                     </Link>
-                    <div class="flex items-center space-x-6 rtl:space-x-reverse">
-                        <a href="tel:5541251234" class="text-sm  text-gray-500 dark:text-white hover:underline">(555) 412-1234</a>
-                        <a href="#" class="text-sm  text-blue-600 dark:text-blue-500 hover:underline">Login</a>
+                    <div className="flex items-center space-x-6 rtl:space-x-reverse">
+                        <a href="tel:5541251234" className="text-sm  text-gray-500 dark:text-white hover:underline">(555) 412-1234</a>
+                        <a href="#" className="text-sm  text-blue-600 dark:text-blue-500 hover:underline">Login</a>
                     </div>
                 </div>
             </nav>
-            <nav class="bg-gray-50 dark:bg-gray-700">
-                <div class="max-w-screen-xl px-4 py-3 mx-auto">
-                    <div class="flex items-center">
-                        <ul class="flex flex-row font-medium mt-0 space-x-8 rtl:space-x-reverse text-sm">
+            <nav className="bg-gray-50 dark:bg-gray-700">
+                <div className="max-w-screen-xl px-4 py-3 mx-auto">
+                    <div className="flex items-center">
+                        <ul className="flex flex-row font-medium mt-0 space-x-8 rtl:space-x-reverse text-sm">
                             <li>
-                                <a href="#" class="text-gray-900 dark:text-white hover:underline" aria-current="page">Home</a>
+                                <a href="#" className="text-gray-900 dark:text-white hover:underline" aria-current="page">Home</a>
                             </li>
                             <li>
-                                <a href="#" class="text-gray-900 dark:text-white hover:underline">Company</a>
+                                <a href="#" className="text-gray-900 dark:text-white hover:underline">Company</a>
                             </li>
                             <li>
-                                <a href="#" class="text-gray-900 dark:text-white hover:underline">Team</a>
+                                <a href="#" className="text-gray-900 dark:text-white hover:underline">Team</a>
                             </li>
                             <li>
-                                <a href="#" class="text-gray-900 dark:text-white hover:underline">Features</a>
+                                <a href="#" className="text-gray-900 dark:text-white hover:underline">Features</a>
                             </li>
                         </ul>
                     </div>
